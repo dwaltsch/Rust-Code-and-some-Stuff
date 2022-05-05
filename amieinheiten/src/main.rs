077426f120a70350109d1226fc2f21d15c37f09d
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-05-05 20:47:27 +0200

    Fahreinheit

---
+++ amieinheiten/src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    println!("How much Fahreinheit");
+loop {
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+    println!("That is {} degrees of freedom", (guess - 30)/2);
+}
+
+}
